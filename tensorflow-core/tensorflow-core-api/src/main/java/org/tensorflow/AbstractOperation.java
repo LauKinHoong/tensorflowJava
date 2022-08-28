7a3d8967998f19a43ffd3db1318cd56bee320c13
LauKinHoong <85264401+LauKinHoong@users.noreply.github.com>
2022-08-28 17:57:20 +0800

    Update AbstractOperation.java
    test maven build

--- tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/AbstractOperation.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/AbstractOperation.java
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-
+// To test maven build
 package org.tensorflow;
 
 import org.bytedeco.javacpp.Pointer;
